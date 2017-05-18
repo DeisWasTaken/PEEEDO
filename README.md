7c71890de723f0b7ee5ba35193a4729c56bd5b95
habobababo <shadowman11221@gmail.com>
2017-05-18 17:55:12 +0200

    Updated README
    Made the config more fancy

--- README.md
+++ README.md
@@ -1,10 +1,47 @@
-# PEEEDO
-Just a gamename
+PEEEDO
+===============
 
-Hooks:
-Shared:
 
-PEDO_Initialize
-PEDO_RoundEnd || returns winner 1 vic , 2 pedo
-PEDO_RoundStart
-PEDO_PreRoundStart
+**PEEEDO** ist nur ein Gamemode
+
+
+
+
+----------
+#### <i class="icon-file"></i> Developer documentation
+-------------
+<i class="icon-th-list">Serverside hooks:
+>
+
+<i class="icon-user">Clientside hooks:
+>
+
+<i class="icon-user"><i class="icon-th-list">Shared hooks:
+
+>
+**PEDO_Initialize** Calls when the gamemode is initialized
+**PEDO_RoundEnd** Calls at the end of the round. **Returns Winner**
+**PEDO_RoundStart** Calls the beginning of the round
+
+
+----------
+#### <i class="icon-pencil"></i> Configuration
+you can configure the gamemode at [*pedocore/gamemode/shared.lua*](https://github.com/habobababo/PEEEDO/blob/master/pedocore/gamemode/shared.lua)
+
+A short list of some configs here
+>PEDO.RoundTime = `300` *x minutes roundtime is legit*
+PEDO.PreRoundTime = `5` -- *the x seconds before the round starts*
+PEDO.PrepareTime = `30` -- *the first round right after mapchange*
+PEDO.SpawnTime = `15` -- *pedobear has another x seconds to spawn*
+PEDO.NotificationTime = `7` -- *the time of the notification*
+PEDO.CatchRadius = `40` --* if a victim comes below that range, he will die.*
+PEDO.PedoWalkSpeed = `250` --*default walkspeed of the pedobear*
+PEDO.VicWalkSpeed = `220` -- *--||-------------------- victim*
+PEDO.RunSpeed = `300` -- *press SHIFT <IN_SPEED> and run!, thats the speed*
+PEDO.PedoSpawnRate = `6` -- *1 pedobears on 6 players. --> with 12 players you got 2 pedobears*
+PEDO.StaminaDrain = `0.01` -- *How fast the stamina drains to zero. 1 is faster, 0.01 is slow*
+PEDO.StaminaRestoreTime =` 0.1`  -- *restoretime. Same as above*
+PEDO.DelayRestoreStamina =` 4` -- *x seconds after release the key, the stamina starts restoring*
+PEDO.StaminaDeadPoint = `10` -- *if stamina is below x, you can't run anymore*
+
+----------
--- pedocore/gamemode/shared.lua
+++ pedocore/gamemode/shared.lua
@@ -1,12 +1,27 @@
 GM.Name = "Pedobear"
-GM.Author = "N/A"
-GM.Email = "N/A"
-GM.Website = "N/A"
+GM.Author = "Habobababo"
+GM.Email = "mail@core-community.de"
+GM.Website = "core-community.de"
+--[[
+
+        ||  JUST EDIT THE MARKED LINES ||
+
+]]--
 
 PEDO = {}
 PEDO.Scoreboard = {}
 
-PEDO.Spawntext = {
+PEDO.PEDO = {}
+PEDO.SPEC = {}
+PEDO.VIC = {}
+
+--[[
+
+        || EDIT BELOW HERE ||
+
+]]--
+
+PEDO.Spawntext = { -- Text in the spawntime of the Pedobear
   "Van wird getankt",
   "Süssichkeiten werden abgepackt",
   "Fell wird gereinigt",
@@ -14,61 +29,65 @@ PEDO.Spawntext = {
   "Luftdruck wird geprueft"
 }
 
-PEDO.RoundTime = 300
-PEDO.PreRoundTime = 5
-PEDO.PrepareTime = 30
+PEDO.RoundTime = 300 -- x minutes roundtime is legit
+PEDO.PreRoundTime = 5 -- the x seconds before the round starts
+PEDO.PrepareTime = 30 -- the first round right after mapchange
+PEDO.SpawnTime = 15 -- Pedobear has another x seconds to spawn
 
-PEDO.SpawnTime = 15
+PEDO.NotificationTime = 7 -- the time of the notification
 
-PEDO.NotificationTime = 7
+PEDO.CatchRadius = 40 -- if a victim comes below that range, he will die.
 
-PEDO.CatchRadius = 40
+PEDO.PedoWalkSpeed = 250 --default walkspeed of the pedobear
+PEDO.VicWalkSpeed = 220 -- --||-------------------- victim
+PEDO.RunSpeed = 300 -- press SHIFT <IN_SPEED> and run!, thats the speed
 
-PEDO.PedoWalkSpeed = 250
-PEDO.VicWalkSpeed = 220
-PEDO.RunSpeed = 300
+PEDO.PedoSpawnRate = 6 -- 1 pedobears on 6 players. --> with 12 players you got 2 pedobears
 
-PEDO.PedoSpawnRate = 6
+PEDO.StaminaDrain = 0.01 -- How fast the stamina drains to zero. 1 is faster, 0.01 is slow
+PEDO.StaminaRestoreTime = 0.1  -- restoretime. Same as above
+PEDO.DelayRestoreStamina = 4 -- x seconds after release the key, the stamina starts restoring
+PEDO.StaminaDeadPoint = 10 -- if stamina is below x, you can't run anymore
 
-PEDO.StaminaDrain = 0.01 -- je höher desto langsamer der drain
-PEDO.StaminaRestoreTime = 0.1
-PEDO.DelayRestoreStamina = 4
-PEDO.StaminaDeadPoint = 10 -- weniger als 10 punkte dann geht nix mehr
-
-PEDO.VIC = {}
-PEDO.VIC.Color = Color(255,0,0,255)
-PEDO.VIC.Name = "Kinder"
-PEDO.VIC.Models = { // Just take 4 Models or Break the Gamemode
+PEDO.VIC.Color = Color(255,0,0,255) -- color of team victim
+PEDO.VIC.Name = "Kinder" -- name of team victim
+PEDO.VIC.Models = { -- NOT IN USE YET --
   ["RIOT"] = "models/player/riot.mdl",
 	["GASMASK"] = "models/player/gasmask.mdl",
 	["URBAN"] = "models/player/urban.mdl",
 	["SWAT"] = "models/player/swat.mdl"
 }
 
-PEDO.VIC.WinTexts = {
+PEDO.VIC.WinTexts = { -- eventtext on endround if team victim wins
   "Die Kinder waren zu schnell!",
   "Die Eltern haben die Kinder gerettet!"
 }
 
-PEDO.PEDO = {}
-PEDO.PEDO.Color = Color(0,255,0,255)
-PEDO.PEDO.Name = "Pedobär"
-PEDO.PEDO.Models = { // Just take 4 Models or Break the Gamemode
+
+PEDO.PEDO.Color = Color(0,255,0,255) -- color of team victim
+PEDO.PEDO.Name = "Pedobär" -- name of team victim
+PEDO.PEDO.Models = { -- NOT IN USE YET --
   ["PHOENIX"] = "models/player/phoenix.mdl",
 	["ARCTIC"] = "models/player/arctic.mdl",
 	["GUERILLA"] = "models/player/guerilla.mdl",
 	["LEET"] = "models/player/leet.mdl"
 }
 
-PEDO.PEDO.WinTexts = {
+PEDO.PEDO.WinTexts = { -- eventtext on endround if team pedo wins
   "Der Pedobär hat es genossen!",
   "Der Pedobär will mehr!",
   "Leider ist kein Kind mehr übrig.."
 }
 
-PEDO.SPEC = {}
-PEDO.SPEC.Name = "Zuschauer"
-PEDO.SPEC.Color = Color(0,70,70,255)
+PEDO.SPEC.Name = "Zuschauer" -- spectator name, -- NOT IN USE --
+PEDO.SPEC.Color = Color(0,70,70,255) -- ...
+
+--[[
+
+        || STOP EDITING HERE ||
+
+]]--
+
 
 function GM:Initialize()
 	hook.Call("PEDO_Initialize")
